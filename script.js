9cdbabee5b025dccca0000b2be25ed767c671246
marolikeo <pissqjerry@gmail.com>
2024-08-22 19:56:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Sélectionne le bouton Shop
+const shopButton = document.querySelector('.shop-button');
+
+// Ajoute une action au clic
+shopButton.addEventListener('click', () => {
+    window.location.href = 'shop.html';  // Redirige vers la page Shop
+});
